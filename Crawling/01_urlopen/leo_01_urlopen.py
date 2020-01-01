38f277b5482fb517ffe2e8d01b876fb13fbda18e
Leo Yang <ylj9707@hotmail.com>
2020-01-01 15:37:03 +1100

    new

--- Crawling/01_urlopen/leo_01_urlopen.py
+++ Crawling/01_urlopen/leo_01_urlopen.py
@@ -4,8 +4,15 @@
 
 import urllib.request
 
+ua_headers = {
+	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"    
+}
+
+
 # send request and get response
-response = urllib.request.urlopen("http://www.baidu.com/")
+request = urllib.request.Request("http://www.baidu.com/", headers = ua_headers)
+
+response = urllib.request.urlopen(request)
 
 # the response support class methods in Python
 html = response.read()
